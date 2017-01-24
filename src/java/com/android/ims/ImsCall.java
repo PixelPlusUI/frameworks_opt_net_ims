b1e2767639bb4a880e7b5c7add0612a40d4b91f6
Ashit Sood <sooda@codeaurora.org>
2017-01-24 11:53:24 -0800

    IMS: Changes for RTT feature
    - Implement changes for RTT feature
    
    Change-Id: I5bafa8af731e8e817c53e0f6087c2fff7e0eeace
    CRs-Fixed: 2178403
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- src/java/com/android/ims/ImsCall.java
+++ src/java/com/android/ims/ImsCall.java
@@ -1792,6 +1792,7 @@ public class ImsCall implements ICall {
         if (mediaProfile.mVideoQuality != ImsStreamMediaProfile.VIDEO_QUALITY_NONE) {
             mediaProfile.mVideoDirection = ImsStreamMediaProfile.DIRECTION_SEND;
         }
+        mediaProfile.mRttMode = mCallProfile.mMediaProfile.mRttMode;
 
         return mediaProfile;
     }
@@ -1810,6 +1811,7 @@ public class ImsCall implements ICall {
         if (mediaProfile.mVideoQuality != ImsStreamMediaProfile.VIDEO_QUALITY_NONE) {
             mediaProfile.mVideoDirection = ImsStreamMediaProfile.DIRECTION_SEND_RECEIVE;
         }
+        mediaProfile.mRttMode = mCallProfile.mMediaProfile.mRttMode;
 
         return mediaProfile;
     }
@@ -2315,6 +2317,25 @@ public class ImsCall implements ICall {
         }
 
         return;
+    }
+
+    /**
+     * Sends RTT Upgrade request
+     *
+     * @param to   : expected profile
+     * @throws CallStateException
+     */
+    public void sendRttModifyRequest(ImsCallProfile to) throws ImsException {
+        logi("RTT: sendRttModifyRequest");
+
+        synchronized(mLockObj) {
+            if (mSession == null) {
+                loge("RTT: sendRttModifyRequest :: no call session");
+                throw new ImsException("No call session",
+                        ImsReasonInfo.CODE_LOCAL_CALL_TERMINATED);
+            }
+            mSession.sendRttModifyRequest(to);
+        }
     }
 
     @VisibleForTesting
